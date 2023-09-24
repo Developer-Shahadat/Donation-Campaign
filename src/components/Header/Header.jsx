0994d7422d0ca17bf61376515b9136862763aebf
Developer-Shahadat <shahadatbci95@gmail.com>
2023-09-25 02:36:18 +0600

    header and banner section & json data create

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import "./Header.css"
+import { NavLink } from "react-router-dom";
+const Header = () => {
+    return (
+        <div className="container mx-auto">
+            <header className="p-4">
+	<div className="container flex justify-between h-16 mx-auto">
+		<a rel="noopener noreferrer" href="/" aria-label="Back to homepage" className="flex items-center p-2">
+			<img src="/src/assets/Logo.png" alt="" />
+		</a>
+		<ul className="items-stretch hidden space-x-3 md:flex">
+			<li className="flex">
+				<NavLink rel="noopener noreferrer" to="/" className=" text-red flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Home</NavLink>
+			</li>
+			<li className="flex">
+				<NavLink rel="noopener noreferrer" to="/donation" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Donation</NavLink>
+			</li>
+			<li className="flex">
+				<NavLink rel="noopener noreferrer" to="/statistics" className="flex items-center px-4 -mb-1 dark:border-transparent ">Statistics</NavLink>
+			</li>
+			
+		</ul>
+		<button className="flex justify-end p-4 md:hidden">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+			</svg>
+		</button>
+	</div>
+    <div className="mt-32 text-center">
+        <h1 className="text-4xl font-bold">I Grow By Helping People In Need</h1>
+    </div>
+    <div className="mt-10">
+        <input className="border-2 p-2 w-1/4 ml-96 " type="text" name="text" placeholder="search" />
+        <button className="p-2 pl-4 pr-4 bg-[#FF444A] text-white font-semibold">Search</button>
+    </div>
+</header>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Header from './components/Header/Header';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>,
+    children : [{
+      
+    }]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

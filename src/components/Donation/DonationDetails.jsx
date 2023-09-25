05d197f64d0ab5e4dd345549d07faf5fb1c2dc28
Developer-Shahadat <shahadatbci95@gmail.com>
2023-09-25 07:42:46 +0600

    sweet alert add

--- src/components/Donation/DonationDetails.jsx
+++ src/components/Donation/DonationDetails.jsx
@@ -1,6 +1,7 @@
 
 import { useLoaderData, useParams } from "react-router-dom";
 
+import Swal from 'sweetalert2';
 
 
 const DonationDetails = () => {
@@ -11,7 +12,24 @@ const DonationDetails = () => {
     console.log(detailsAll);
 
  
-
+    const handleDonateClick = () => {
+        Swal.fire({
+          title: 'Donation Confirmation',
+          text: `Are you sure you want to donate $${detailsAll.price}?`,
+          icon: 'question',
+          showCancelButton: true,
+          confirmButtonText: 'Yes, donate!',
+          cancelButtonText: 'Cancel',
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            // Handle donation logic here
+            // You can perform the actual donation or redirect the user to a payment page
+            // For example: alert('Donation successful!');
+            Swal.fire('Donation successful!', '', 'success');
+          }
+        });
+      };
     
         
     return (
@@ -20,7 +38,7 @@ const DonationDetails = () => {
                         <figure><img className="w-[80%]" src={detailsAll.img} /></figure>
                         <div className="relative -mt-14 ml-20">
                            <div className="w-[531px] p-1 bg-opacity-70 bg-black">
-                           <button className="btn bg-[#FF444A;] rounded-lg border-none text-white hover:bg-red-400">Donate  ${detailsAll.price}</button>
+                           <button onClick={handleDonateClick} className="btn font-bold" style={{backgroundColor: detailsAll.btn_bg_color,color:detailsAll.category_bg_color}}>Donate  ${detailsAll.price}</button>
                            </div>
                         </div>
                         <div className="card-body">
